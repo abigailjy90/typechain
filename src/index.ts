4e2310eec41698519786e53ad6f79e67011636ec
abigailjy90 <70828921+abigailjy90@users.noreply.github.com>
2021-09-12 11:53:51 +0900

    Interfaces on Typescript2

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,21 @@
-// argument type & function type
-const sayHi = (name: string, age: number, gender: string): string => {
-  return `Hello ${name}, you are ${age}, you are a ${gender}!`;
+interface Human {
+  name: string;
+  age: number;
+  gender: string;
+}
+
+const person = {
+  name: "lala",
+  age: 22,
+  gender: "female",
 };
 
-console.log(sayHi("Jessica", 25, "female"));
+// argument type & function type
+// 'person' object has 'Human' interface
+const sayHi = (person: Human): string => {
+  return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}!`;
+};
+
+console.log(sayHi(person));
 
 export {};
