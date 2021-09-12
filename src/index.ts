a742ce38297925478ae3e79bb6f2200cc0503798
abigailjy90 <70828921+abigailjy90@users.noreply.github.com>
2021-09-12 12:36:52 +0900

    Blockchain Creating a Block1

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,29 @@
-// private & public variable
-// interface 대신에 class를 이용하면 js로 컴파일 됨. 상황에 맞게 활용 가능.
-class Human {
-  public name: string;
-  public age: number;
-  public gender: string;
-  constructor(name: string, age: number, gender: string) {
-    this.name = name;
-    this.age = age;
-    this.gender = gender;
+class Block {
+  public index: number;
+  public hash: string;
+  public previousHash: string;
+  public data: string;
+  public timestamp: number;
+  constructor(
+    index: number,
+    hash: string,
+    previousHash: string,
+    data: string,
+    timestamp: number
+  ) {
+    this.index = index;
+    this.hash = hash;
+    this.previousHash = previousHash;
+    this.data = data;
+    this.timestamp = timestamp;
   }
 }
 
-const lynn = new Human("Lynn", 18, "female");
+const genesisBlock: Block = new Block(0, "20320320", "", "Hello", 123456);
 
-// argument type & function type
-const sayHi = (person: Human): string => {
-  return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}!`;
-};
+// js와 다르게 type을 지정했기 때문에 Block배열만 넣을 수 있음.
+let blockchain: [Block] = [genesisBlock];
 
-console.log(sayHi(lynn));
+console.log(blockchain);
 
 export {};
