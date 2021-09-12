b94344cc88b75e7c1734a447560f78ca9de581a9
abigailjy90 <70828921+abigailjy90@users.noreply.github.com>
2021-09-12 12:10:52 +0900

    Classes on Typescript

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,23 @@
-interface Human {
-  name: string;
-  age: number;
-  gender: string;
+// private & public variable
+// interface 대신에 class를 이용하면 js로 컴파일 됨. 상황에 맞게 활용 가능.
+class Human {
+  public name: string;
+  public age: number;
+  public gender: string;
+  constructor(name: string, age: number, gender: string) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+  }
 }
 
-const person = {
-  name: "lala",
-  age: 22,
-  gender: "female",
-};
+const lynn = new Human("Lynn", 18, "female");
 
 // argument type & function type
-// 'person' object has 'Human' interface
 const sayHi = (person: Human): string => {
   return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}!`;
 };
 
-console.log(sayHi(person));
+console.log(sayHi(lynn));
 
 export {};
